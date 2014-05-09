f29ff9af11934cf09c08569b0d4644838e5095c7
Alex Deibel <deibelal@msu.edu>
2014-05-09 11:22:55 -0400

    added conversion from nuc to grav units

--- ns/ns_def.py
+++ ns/ns_def.py
@@ -17,7 +17,22 @@ class ns:
         self.luminosity = np.empty(0)
         self.specific_heat = np.empty(0)
         
+        
 	def _tov_eqns(self):
 
+        tolerance_in_r = 1.0e-4 
+        tolerance_in_p = 1.0e-4
+        
+		# controls
+		rtol = 1.0e-6
+		atol = 1.0e-7
+
+		core_step = 1.0e4	! cm
+		crust_step = 0.05	! variation in lnP
 	
+		# boundary conditions 
+		T_ePhi = 0.01
+		P_start = 160.0
+		P_end = 1.0e-6
+		Phi_start = 0.0	
 
