c7803e115870104a1f302838f511df6929610621
Alex Deibel <deibelal@msu.edu>
2014-05-09 11:09:29 -0400

    added thermal variables to ns_def.py

--- ns/ns_def.py
+++ ns/ns_def.py
@@ -4,12 +4,18 @@ class ns:
     
     def __init__(self):
 
+		# basic variables
 		self.n_interior_pts = np.empty(0)
         self.mass = np.empty(0)
         self.baryonic_mass = np.empty(0)
         self.radius = np.empty(0)
         self.grav_potential = np.empty(0)
         self.inertia = np.empty(0)
+        
+        # thermal variables 
+        self.temp = np.empty(0)
+        self.luminosity = np.empty(0)
+        self.specific_heat = np.empty(0)
         
 	def _tov_eqns(self):
 
